23e06b6c0e381f4cb7135482bee68fb92e101b92
Jaden Suh <jadenjs1@uci.edu>
2026-06-05 16:34:20 -0700

    most goated door update

--- Assets/Scripts/Doors/DoorResetter.cs
+++ Assets/Scripts/Doors/DoorResetter.cs
@@ -15,44 +15,42 @@ public class DoorResetter : MonoBehaviour
     private Transform handle1;
     [SerializeField]
     private Transform handle2;
-    private Vector3 startPositionDoor1;
+    [SerializeField]
+    private Rigidbody handleRb1;
+    [SerializeField]
+    private Rigidbody handleRb2;
     private Quaternion startRotationDoor1;
-    private Vector3 startPositionDoor2;
     private Quaternion startRotationDoor2;
-    private Vector3 startPositionHandle1;
+    private Vector3 startPoisitionHandle1;
+    private Vector3 startPoisitionHandle2;
     private Quaternion startRotationHandle1;
-    private Vector3 startPositionHandle2;
     private Quaternion startRotationHandle2;
 
     void Start()
     {
         // Store the default position and rotation at the start
-        startPositionDoor1 = door1.position;
         startRotationDoor1 = door1.rotation;
-        startPositionDoor2 = door2.position;
         startRotationDoor2 = door2.rotation;
-        startPositionHandle1 = handle1.position;
         startRotationHandle1 = handle1.rotation;
-        startPositionHandle2 = handle2.position;
         startRotationHandle2 = handle2.rotation;
+        startPoisitionHandle1 = handle1.position;
+        startPoisitionHandle2 = handle2.position;
     }
     public void resetDoors()
     {
         rb1.isKinematic = true;
         rb2.isKinematic = true;
-        door1.position = startPositionDoor1;
+        handleRb1.isKinematic = true;
+        handleRb2.isKinematic = true;
         door1.rotation = startRotationDoor1;
-        door2.position = startPositionDoor2;
         door2.rotation = startRotationDoor2;
-        handle1.position = startPositionHandle1;
+        handle1.position = startPoisitionHandle1;
+        handle2.position = startPoisitionHandle2;
         handle1.rotation = startRotationHandle1;
-        handle2.position = startPositionHandle2;
         handle2.rotation = startRotationHandle2;
         rb1.isKinematic = false;
         rb2.isKinematic = false;
-        rb1.linearVelocity = Vector3.zero;
-        rb2.linearVelocity = Vector3.zero;  
-        rb1.angularVelocity = Vector3.zero;
-        rb2.angularVelocity = Vector3.zero;
+        handleRb1.isKinematic = false;
+        handleRb2.isKinematic = false;
     }
 }
